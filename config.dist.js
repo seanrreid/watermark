745d294bbc717ede10e4575311f903d73a086bce
Sean R Reid <sean@torchcodelab.com>
2021-08-27 12:11:04 -0400

    moved constants to a config file

---
+++ config.dist.js
@@ -0,0 +1,8 @@
+const LOGO = '/images/static/[YOUR_LOGO_FILE_HERE]'; // The logo to be used for the watermark
+const LOGO_MARGIN_PERCENTAGE = 3; // margin percentage, integer
+const MAXHEIGHT = 960; // max-height
+const MAXWIDTH = 960; // max-width
+
+const TEXTDATA = {
+    text: '', //the text to be rendered on the image
+};
--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const path = require("path"),
-  fs = require("fs"),
-  watermark = require("./watermark");
+const path = require('path'),
+    fs = require('fs'),
+    watermark = require('./watermark');
+
+// CONSTANTS
+const {
+    LOGO,
+    LOGO_MARGIN_PERCENTAGE,
+    MAXHEIGHT,
+    MAXWIDTH,
+    TEXTDATA,
+} = require('./config');
 
 const timeStamp = Date.now();
 
-const sourcePath = path.join(__dirname, "images/source"),
-  destinationPath = path.join(__dirname, `images/converted-${timeStamp}`);
-
-// CONSTANTS
-const LOGO = "./images/static/watermark_slc.png";
-const LOGO_MARGIN_PERCENTAGE = 3;
-const MAXHEIGHT = 960;
-const MAXWIDTH = 960;
-
-const TEXTDATA = {
-  text: "© Smells Like Cheese", //the text to be rendered on the image
-};
+const sourcePath = path.join(__dirname, 'images/source'),
+    destinationPath = path.join(__dirname, `images/converted-${timeStamp}`);
 
 fs.readdir(sourcePath, (error, files) => {
-  if (error) throw error;
-  files.map((file) => {
-    file !== ".DS_Store" && file !== ".gitignore"
-      ? watermark(`${sourcePath}/${file}`, LOGO_MARGIN_PERCENTAGE, LOGO, TEXTDATA).then(
-          (image) => {
-            console.log("Watermarking image", file);
-            image.scaleToFit(MAXHEIGHT, MAXWIDTH);
-            image.write(`${destinationPath}/${file}`);
-          }
-        )
-      : null;
-  });
+    if (error) throw error;
+    files.map((file) => {
+        file !== '.DS_Store' && file !== '.gitignore'
+            ? watermark(
+                  `${sourcePath}/${file}`,
+                  LOGO_MARGIN_PERCENTAGE,
+                  LOGO,
+                  TEXTDATA
+              ).then((image) => {
+                  console.log('Watermarking image', file);
+                  image.scaleToFit(MAXHEIGHT, MAXWIDTH);
+                  image.write(`${destinationPath}/${file}`);
+              })
+            : null;
+    });
 });
